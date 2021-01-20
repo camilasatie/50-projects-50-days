95991e6d134f30d260fb14b0b5541056a40d1cca
Camila Satie <camilasatie.dev@gmail.com>
2021-01-20 10:25:47 -0300

    add initial drink-water

---
+++ drink-water/script.js
@@ -0,0 +1,24 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const lister = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const reimaned = document.getElementById("reimaned");
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener("click", () => highlightCups(index));
+});
+
+function highlightCups(index) {
+  if (
+    smallCups[index].classList.contains("full") &&
+    !smallCups[index].nextElementSibling.classList.contains("full")
+  ) {
+    index--;
+  }
+  smallCups.forEach((cup, index2) => {
+    if (index2 <= index) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
