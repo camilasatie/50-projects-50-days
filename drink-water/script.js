f0b186acb383262236d6c3417b5cdfcb21d96106
Camila Satie <camilasatie.dev@gmail.com>
2021-01-21 08:34:22 -0300

    add update big cup

--- drink-water/script.js
+++ drink-water/script.js
@@ -1,7 +1,9 @@
 const smallCups = document.querySelectorAll(".cup-small");
-const lister = document.getElementById("liters");
+const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
-const reimaned = document.getElementById("reimaned");
+const remained = document.getElementById("remained");
+
+updateBigCup();
 
 smallCups.forEach((cup, index) => {
   cup.addEventListener("click", () => highlightCups(index));
@@ -21,4 +23,29 @@ function highlightCups(index) {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}`;
+  }
 }
